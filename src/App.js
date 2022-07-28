d76bc2f063e2258c937f85d6eded0aa2958c7d3e
md-akram <akramkaikobad@gmail.com>
2022-07-28 19:35:36 +0600

    created single post page markup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import TopBar from './components/topbar/TopBar'
 import Home from './pages/home/Home'
 import './app.css'
+import Single from './pages/single/Single'
 
 const App = () => {
     return (
         <>
             <TopBar />
             <Home />
+            <Single />
         </>
     )
 }
---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,29 @@
+import './single.css'
+
+const Single = () => {
+    return (
+        <div className="single">
+            <img src="" alt="" className="single-img" />
+            <h1 className="single-title">
+                Lorem ipsum dolor sit amet.
+                <div className="single-edit">
+                    <i class="far fa-edit"></i>
+                    <i class="far fa-trash-alt"></i>
+                </div>
+            </h1>
+            <div className="single-info">
+                <span className="single-author">
+                    Author: <b>akram</b>
+                </span>
+                <span className="single-date">
+                    10 mins ago
+                </span>
+            </div>
+            <p className="single-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit blanditiis aut quae ab provident vel labore soluta, cum adipisci dicta cumque dolore accusamus hic, rerum similique dolor, consequuntur aliquam esse ea error sequi! Ea facere, at labore culpa fugiat repellat autem omnis nostrum hic soluta aliquid obcaecati delectus, sequi nihil odit unde voluptatem provident tempora. Eligendi labore ipsum, debitis doloremque fugit reiciendis accusantium iste at pariatur. Rerum labore, eos nam sapiente veniam ipsum architecto cum quis debitis atque totam sunt deleniti voluptate laudantium unde excepturi delectus. Ullam blanditiis corporis cum atque earum iure totam ad similique in accusantium incidunt facilis unde tempore harum repellat quod, commodi dolores quasi saepe molestias maiores! Repellendus obcaecati ad accusantium, molestias facere eos perspiciatis aliquam, laudantium a doloremque esse nulla libero, tenetur aliquid reprehenderit magni! Error perspiciatis vel culpa non quaerat excepturi unde. Doloremque sapiente, quos odit deserunt a est in ratione ducimus sint fugiat tempore explicabo quo? Eaque dolorem laborum accusantium voluptatum culpa, sed vitae nihil sint voluptatem maiores dolor fuga expedita, cupiditate magnam corrupti ipsum facere debitis voluptates optio tempore reprehenderit illo doloribus. Assumenda ullam qui nulla autem fugit voluptatum ut dicta! Saepe libero laboriosam dolore dolorem mollitia rerum ad, autem possimus, eaque recusandae repellendus quae, doloremque maxime nihil doloribus a distinctio et non! Voluptas minus deserunt dignissimos sequi, molestias dolores. Incidunt, quam nemo, officiis dolore, ratione laborum officia earum aperiam veritatis autem corrupti ut qui! Totam laboriosam in obcaecati fugiat! Libero voluptatem reprehenderit quis veniam dicta earum minima fugiat quaerat odit nihil optio ea temporibus ipsam tenetur praesentium quidem facere itaque ut explicabo, incidunt est placeat ab corporis obcaecati! Iusto reiciendis fugit, nam veritatis possimus, veniam sequi laboriosam inventore tenetur magnam voluptatum neque. Quasi possimus reiciendis facere illum animi harum earum delectus quam vel ab. Hic autem enim repellat incidunt mollitia perspiciatis recusandae quas pariatur. Delectus dolor nihil architecto vitae cumque, voluptatum nemo nesciunt numquam quidem qui, a dicta. Natus, harum quos a accusantium assumenda labore maiores repellendus aliquid animi? Id quis voluptate aperiam ratione veniam est aliquam tempore reiciendis nesciunt, modi magnam cupiditate, sit et, autem perferendis hic in illum ex eius quo quas. Aut laudantium, sint facere aliquam non iure omnis accusamus deserunt officia, corporis unde voluptates dolores perferendis possimus illum, exercitationem porro. Sit libero quae quos impedit, facere, porro ipsum officia magnam aliquid iusto, molestiae recusandae unde. Velit ipsam iste non nesciunt itaque neque aspernatur quidem ratione porro repellat ipsum minus ut nobis quaerat iure, cumque laborum recusandae a assumenda expedita repellendus sapiente totam aperiam vitae. Quis natus esse quos qui aspernatur laborum sit assumenda atque fugit inventore consequatur aliquid a, amet harum adipisci? Accusamus voluptate alias ratione aperiam illo enim nisi dolorum omnis natus, unde nam sapiente nihil quo cumque modi ipsam quis blanditiis nesciunt magnam? Perferendis officiis modi quidem doloremque nulla, alias quisquam repellat dicta, neque nesciunt molestiae earum obcaecati! Pariatur doloremque maiores, praesentium quia blanditiis non accusamus ducimus hic at eos repellendus dolore? Sequi facere numquam totam temporibus officia, excepturi corrupti, beatae obcaecati amet, porro repellendus!
+            </p>
+        </div>
+    )
+}
+
+export default Single
